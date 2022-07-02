483aece2ae1541cc68acaba761a048dc7a9f131c
Criss <cristinaguzman@live.cl>
2022-07-02 18:15:34 -0400

    Tarea 7 Ricky y Morty

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const resourceUrl = "https://rickandmortyapi.com/api/character/";
+
+axios
+  .get(resourceUrl)
+  .then(({ data: { results } }) => {
+    const html = results.map(
+      (item) =>
+        `<li>
+        <img src="${item.image}" alt="" />
+        ${item.name}
+      </li>`
+    );
+
+    $(".character-list")[0].innerHTML = html.join("");
+  })
+  .catch((err) => console.log(err));
+
+/*
+const handleErrors = (response) => {
+  if (!response.ok) console.error(`${response.status}: ${response.statusText}`);
+  return response.json();
+}
+
+fetch(url)
+  .then(handleErrors)
+  .then(({ results }) => {
+    const html = results.map(character =>
+      `<li>
+        <img src="${character.image}" alt="" />
+        ${character.name}
+      </li>`
+    );
+
+    $('.character-list')[0].innerHTML = html.join('');
+  })
+  .catch(err => console.log(err));
+*/
+
+/*
+$.ajax({
+  url,
+  success: ({ results }) => {
+    const html = results.map(character =>
+      `<li>
+        <img src="${character.image}" alt="" />
+        ${character.name}
+      </li>`
+    );
+
+    $('.character-list').html(html);
+  },
+  error: (err) => {
+    console.log(err.statusText);
+  }
+});
+*/
